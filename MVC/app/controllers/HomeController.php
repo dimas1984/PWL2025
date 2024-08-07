452656de70797abebc5c621683883430bcf47a96
dimas wahyu <dimas.w@polinema.ac.id>
2024-08-07 14:36:02 +0700

    Memperbaiki error pada MVC

--- MVC/app/controllers/HomeController.php
+++ MVC/app/controllers/HomeController.php
@@ -10,7 +10,7 @@ class HomeController
     }
 
     public function index(){
-        $user = $this->userModel->getAllUsers();
+        $users = $this->userModel->getAllUsers();
         require '../app/views/home.php';
     }
 }
